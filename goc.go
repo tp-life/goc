a569267e6764f7f69e6a9eea3a1e2a82105bbafe
tangping <pin.r@outlook.com>
2021-08-04 21:55:02 +0800

    bash->sh

---
+++ goc.go
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-04 21:27:46
+ * @LastEditTime: 2021-08-04 21:51:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \goc-master\goc.go
+ */
+package main
+
+import "github.com/wokill/goc/cmd"
+
+func main() {
+	cmd.Execute()
+}
